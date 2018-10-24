b1fef0217de9c2a829a3bc74f569faf2e802d5de
Jerry-T-Kane <42736118+Jerry-T-Kane@users.noreply.github.com>
2018-10-24 17:39:53 +1100

    config

--- TableTap/UL/AdminHome.aspx.designer.cs
+++ TableTap/UL/AdminHome.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace TableTap.UL {
         protected global::System.Web.UI.WebControls.Button addUserButton;
         
         /// <summary>
+        /// scheduled control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button scheduled;
+        
+        /// <summary>
         /// qrButton control.
         /// </summary>
         /// <remarks>
