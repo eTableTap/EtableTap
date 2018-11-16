73ef69be34aeb2a899c239b88b0cefb30098c236
Jerry-T-Kane <42736118+Jerry-T-Kane@users.noreply.github.com>
2018-11-16 20:22:03 +1100

    comments

--- TableTap/BusinessLayer/Classes/TableBL.cs
+++ TableTap/BusinessLayer/Classes/TableBL.cs
@@ -9,24 +9,40 @@ namespace TableTap.BusinessLayer.Classes
 {
     public class TableBL
     {
-        public static List<TableModel> FillTableList(int id)
+
+
+        /// <summary>
+        /// searches for all tables within associated building by ID via TableDAL.LoadTableList(buildingID);
+        /// Returns list of TableModels
+        /// </summary>
+        public static List<TableModel> FillTableList(int buildingID)
         {
             List<TableModel> tables = new List<TableModel>();
 
-            tables = TableDAL.LoadTableList(id);
+            tables = TableDAL.LoadTableList(buildingID);
 
             return tables;
         }
-        public static TableModel GetTableByID(int id)
+
+
+        /// <summary>
+        /// searches for a table by tableID via TableDAL.LoadTableByID(tableID);
+        /// returns tableModel
+        /// </summary>
+        public static TableModel GetTableByID(int tableID)
         {
             TableModel table = new TableModel();
 
-            table = TableDAL.LoadTableByID(id);
+            table = TableDAL.LoadTableByID(tableID);
 
             return table;
         }
 
 
+        /// <summary>
+        /// checks avalibility of table by tableID, Hour and Date via TableDAL.CheckTableHourAvailability(TableID, Hour, dateTime);
+        /// returns bool if avaliable or not
+        /// </summary>
         public static bool CheckTableHourAvailability(int TableID, int Hour, DateTime dateTime)
         {
             bool bCheckHour = TableDAL.CheckTableHourAvailability(TableID, Hour, dateTime);
@@ -38,15 +54,19 @@ namespace TableTap.BusinessLayer.Classes
             return false;
         }
 
-   
+        /// <summary>
+        /// adds a new table by tableModel via TableDAL.AddNewTable(table);
+        /// </summary>
         public static void ProcessAddNewTable(TableModel table)
         {
             TableModel newTable = table;
             try
-            { }
+            {
+                TableDAL.AddNewTable(table);
+            }
             catch
             { }
-            TableDAL.AddNewTable(table);
+            
 
         }
     }
