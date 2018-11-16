bce34c117c2e371a71041dd3317437586ed80eae
Elizabeth Capararo <elizabeth.capararo@gmail.com>
2018-11-16 19:22:44 +1100

    UL - Account commented

--- TableTap/UL/Account.aspx.cs
+++ TableTap/UL/Account.aspx.cs
@@ -5,6 +5,15 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+/* 
+    INFT 3970 - IT Major Project - Implementation
+    Hayden Bartlett – C3185636
+    Beau Maund – C3163068
+
+    Source File Purpose:
+    - Simply checks if a user is logged in, and if found to not be the case; redirects.
+ */
+
 namespace TableTap.UL
 {
     public partial class Account : System.Web.UI.Page
