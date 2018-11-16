f6283befabf71c7bd6f8719f0e3602f24f8ac1a1
Elizabeth Capararo <elizabeth.capararo@gmail.com>
2018-11-16 21:56:21 +1100

    SiteMaster commented

--- TableTap/UL/Site.Master.cs
+++ TableTap/UL/Site.Master.cs
@@ -11,9 +11,10 @@ namespace TableTap.UL
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // session will make differt options show up on the nav bar ---- need to add admin
+            //Session will make differt options show up on the nav bar
             if ((string)Session["loggedUser"] == "user")
             {
+                //Standard user identifier
                 loginNavigate.Visible = false;
                 adminNavigate.Visible = false;
                 logoutNavigate.Visible = true;
@@ -22,6 +23,7 @@ namespace TableTap.UL
             }
             else if ((string)Session["loggedUser"] == "admin")
             {
+                //Administrator user identifier
                 loginNavigate.Visible = false;
                 adminNavigate.Visible = true;
                 logoutNavigate.Visible = true;
@@ -30,6 +32,7 @@ namespace TableTap.UL
             }
             else
             {
+                //Non-identifiable type
                 loginNavigate.Visible = true;
                 logoutNavigate.Visible = false;
                 accountNavigate.Visible = false;
