531c75e39ff69ac2d77ca43ce050122c907b01bc
Jerry-T-Kane <42736118+Jerry-T-Kane@users.noreply.github.com>
2018-11-16 18:06:12 +1100

    comments.

--- TableTap/BackGroundWorker/Classes/EmailQueuing.cs
+++ TableTap/BackGroundWorker/Classes/EmailQueuing.cs
@@ -45,79 +45,23 @@ namespace TableTap.BackGroundWorker.Classes
         }
 
 
-        public static List<List<string>> testData()
-        {
-            /// Test data
-
-                        string email1 = "hayden.bartlett1@hotmail.com";
-                        string phone = "0434346773";
-                        string user = "Hayden Bartlett";
-                        string tableid = "001";
-                        string roomName = "room1";
-                        string buildName = "building1";
-                        string date = "09/09/1864";
-                        string hour = "9.30";
-                        string URL = "www.URL.com";
-
-
-                        string email2 = "c3185636@uon.edu.au";
-                        string phone2 = "0434346773";
-                        string user2 = "hayden bartlett2";
-                        string tableid2 = "001";
-                        string roomName2 = "room1";
-                        string buildName2 = "building1";
-                        string date2 = "09/09/1864";
-                        string hour1 = "9.30";
-                        string URL2 = "www.URL.com";
-
-            //for each loop through SQL results 
-
-
-                        List<string> dataset1 = new List<string>();
-                        List<string> dataset2 = new List<string>();
-
-
-                        dataset1.Add(user);
-                        dataset1.Add(phone);
-                        dataset1.Add(email1);
-                        dataset1.Add(tableid);
-                        dataset1.Add(roomName);
-                        dataset1.Add(buildName);
-                        dataset1.Add(date);
-                        dataset1.Add(hour);
-                        dataset1.Add(URL);
-
-
-                        dataset2.Add(user2);
-                        dataset2.Add(phone2);
-                        dataset2.Add(email2);
-                        dataset2.Add(tableid2);
-                        dataset2.Add(roomName2);
-                        dataset2.Add(buildName2);
-                        dataset2.Add(date2);
-                        dataset2.Add(hour1);
-                        dataset2.Add(URL2);
-
-
-
-                        List<List<string>> data = new List<List<string>>();
-                        data.Add(dataset1);
-                        data.Add(dataset2);
-
-                       return data;
-        }
-
 
         public static void startEmailWorker()
         {
+            // main Email worker, searches for any bookings in the next hour
+            // via the booking table, searches user, room and building tables for associated data
+            // loops through each booking sending email notifications via NotifyBL
+            
+
+
             List<BookingModel> groups = new List<BookingModel>();
 
-//            List<List<string>> data = new List<List<string>>();
 
             groups = BookingDAL.loadBookingListattime();
 
 
-            foreach(BookingModel booking in groups)
+            // loops through each booking returned by BookingDAL.loadBookingListattime();
+            foreach (BookingModel booking in groups)
             {
                 List<string> dataset1 = new List<string>();
 
@@ -198,28 +142,9 @@ namespace TableTap.BackGroundWorker.Classes
                 }
             }
 
-
-  //          return data;
         }
 
 
-
-        public static void testemailWorker()
-        {
-            DateTime time = System.DateTime.Now;
-
-            List<List<string>> bookings = testData();
-
-            foreach(List<string> booking in bookings)
-            {
-                NotifyBL.startNotifyBooking(booking[0], booking[1], booking[2], booking[3], booking[4], booking[5], booking[6], booking[7], booking[8]);
-            }
-
-
-
-            
-
-        }
 
 
 
